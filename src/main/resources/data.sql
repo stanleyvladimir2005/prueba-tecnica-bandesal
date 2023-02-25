09e41dd24bebf7594d2b13f011597eea7b83a835
Vladimir Stanley Alvarez <stanleyvladimir2005@hotmail.com>
2023-02-25 12:03:14 -0600

    Cambio a gradle 8.0

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,10 +4,10 @@ CREATE SEQUENCE blog_seq
     NOCACHE;
 
 CREATE TABLE blog (
-                      id NUMBER(8) NOT NULL,
-                      title VARCHAR2(50) NOT NULL,
-                      description VARCHAR2(400) NOT NULL,
-                      CONSTRAINT blog_pk PRIMARY KEY (id)
+  id NUMBER(8) NOT NULL,
+  title VARCHAR2(50) NOT NULL,
+  description VARCHAR2(400) NOT NULL,
+   CONSTRAINT blog_pk PRIMARY KEY (id)
 );
 
 CREATE SEQUENCE reader_seq
@@ -16,9 +16,9 @@ CREATE SEQUENCE reader_seq
     NOCACHE;
 
 CREATE TABLE reader (
-                        id NUMBER(8) NOT NULL,
-                        name VARCHAR2(8) NOT NULL,
-                        CONSTRAINT reader_pk PRIMARY KEY (id)
+   id NUMBER(8) NOT NULL,
+   name VARCHAR2(8) NOT NULL,
+   CONSTRAINT reader_pk PRIMARY KEY (id)
 );
 
 CREATE SEQUENCE blog_reader_seq
@@ -27,10 +27,10 @@ CREATE SEQUENCE blog_reader_seq
     NOCACHE;
 
 CREATE TABLE blog_reader (
-                             b_id NUMBER(8) NOT NULL,
-                             r_id NUMBER(8) NOT NULL,
-                             CONSTRAINT blog_reader_fk1 FOREIGN KEY (b_id) REFERENCES blog(id),
-                             CONSTRAINT blog_reader_fk2 FOREIGN KEY (r_id) REFERENCES reader(id)
+    b_id NUMBER(8) NOT NULL,
+    r_id NUMBER(8) NOT NULL,
+    CONSTRAINT blog_reader_fk1 FOREIGN KEY (b_id) REFERENCES blog(id),
+    CONSTRAINT blog_reader_fk2 FOREIGN KEY (r_id) REFERENCES reader(id)
 );
 
 CREATE SEQUENCE users_seq
@@ -40,13 +40,13 @@ CREATE SEQUENCE users_seq
 
 
 CREATE TABLE users (
-        id NUMBER(19) NOT NULL,
-        name VARCHAR2(50) NOT NULL,
-        firstname VARCHAR2(50) NOT NULL,
-        lastname VARCHAR2(50) NOT NULL,
-        email VARCHAR2(100) NOT NULL,
-        password VARCHAR2(100) NOT NULL,
-        CONSTRAINT user_pk PRIMARY KEY (id)
+    id NUMBER(19) NOT NULL,
+    name VARCHAR2(50) NOT NULL,
+    firstname VARCHAR2(50) NOT NULL,
+    lastname VARCHAR2(50) NOT NULL,
+    email VARCHAR2(100) NOT NULL,
+    password VARCHAR2(100) NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id)
 );
 
 CREATE SEQUENCE roles_seq
@@ -55,9 +55,9 @@ CREATE SEQUENCE roles_seq
     NOCACHE;
 
 CREATE TABLE roles (
-     id NUMBER (19) NOT NULL,
-     name VARCHAR2(50) NOT NULL,
-     CONSTRAINT role_pk PRIMARY KEY (id)
+    id NUMBER (19) NOT NULL,
+    name VARCHAR2(50) NOT NULL,
+    CONSTRAINT role_pk PRIMARY KEY (id)
 );
 
 CREATE SEQUENCE users_roles_seq
@@ -66,8 +66,8 @@ CREATE SEQUENCE users_roles_seq
     NOCACHE;
 
 CREATE TABLE users_roles (
-                             user_id NUMBER NOT NULL,
-                             role_id NUMBER NOT NULL,
-                             CONSTRAINT user_role_fk1 FOREIGN KEY (user_id) REFERENCES users (id),
-                             CONSTRAINT user_role_fk2 FOREIGN KEY (role_id) REFERENCES roles (id)
+    user_id NUMBER NOT NULL,
+    role_id NUMBER NOT NULL,
+    CONSTRAINT user_role_fk1 FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT user_role_fk2 FOREIGN KEY (role_id) REFERENCES roles (id)
 );
